faeda4abf27efb7ebd6b81a3381c46de6b58ebd2
Yash Ladlapure <yash.ladlapure@gmail.com>
2026-01-02 19:51:15 +0530

    fix: Correct TypeScript component imports and service calls

--- frontend/src/app/challenge-solved-notification/challenge-solved-notification.component.ts
+++ frontend/src/app/challenge-solved-notification/challenge-solved-notification.component.ts
@@ -4,12 +4,12 @@
  */
 
 import { TranslateService, TranslateModule } from '@ngx-translate/core'
-import { ChallengeService } from '../Services/challenge.service'
-import { ConfigurationService } from '../Services/configuration.service'
-import { ChangeDetectorRef, Component, NgZone, type OnInit, inject } from '@angular/core'
-import { CookieService } from 'ngx-cookie'
-import { CountryMappingService } from '../Services/country-mapping.service'
-import { SocketIoService } from '../Services/socket-io.service'
+import { ChallengeService } from '../services/challenge.service'
+import { ConfigurationService } from '../services/configuration.service'
+import { ChangeDetectorRef, Component, NgZone, type OnInit, Inject } from '@angular/core'
+import { CookieService } from 'ngx-cookie-service'
+import { CountryMappingService } from '../services/country-mapping.service'
+import { SocketIoService } from '../services/socket-io.service'
 import { ClipboardModule } from 'ngx-clipboard'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
@@ -23,9 +23,7 @@ interface ChallengeSolvedNotification {
   key: string
   message: string
   flag: string
-  country ?: {  code : string ,  name : string  }
-  copied : boolean
-  codingChallengeId ?: string
+  copied: false
 }
 
 @Component({
@@ -33,69 +31,69 @@ interface ChallengeSolvedNotification {
   templateUrl: './challenge-solved-notification.component.html',
   styleUrls: ['./challenge-solved-notification.component.scss'],
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule, ClipboardModule, TranslateModule, LowerCasePipe]
+  imports: [
+    TranslateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule,
+    ClipboardModule,
+    LowerCasePipe
+  ]
 })
 export class ChallengeSolvedNotificationComponent implements OnInit {
   notifications: ChallengeSolvedNotification[] = []
   showCtfFlagsInNotifications = false
-  showCtfCountryDetailsInNotifications = 'none'
-  private challengeService = inject(ChallengeService)
-  private configurationService = inject(ConfigurationService)
-  private ref = inject(ChangeDetectorRef)
-  private ngZone = inject(NgZone)
-  private socketIoService = inject(SocketIoService)
-  private countryMappingService = inject(CountryMappingService)
-  private translateService = inject(TranslateService)
-  private cookieService = inject(CookieService)
-  private router = inject(Router)
+  showCTFCountryDetailsInNotifications = false
 
-  ngOnInit () {
+  constructor(
+    private challengeService: ChallengeService,
+    private configurationService: ConfigurationService,
+    private ref: ChangeDetectorRef,
+    private ngZone: NgZone,
+    private socketIoService: SocketIoService,
+    private countryMappingService: CountryMappingService,
+    private translateService: TranslateService,
+    private cookieService: CookieService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
     this.socketIoService.on('challengeSolved', (challenge) => {
-      if (challenge && challenge.key) {
-        this.ngZone.run(async () => {
-          try {
-            const solvedChallenge = await firstValueFrom(this.challengeService.getChallenge(challenge.key))
-            const challengeComplete = { ...challenge, ...solvedChallenge }
-            const countryInfo = this.countryMappingService.map(challengeComplete.country)
-            const message = await firstValueFrom(
+      this.ngZone.run(async () => {
+        try {
+          const solvedChallenge = await firstValueFrom(this.challengeService.getChallenge(challenge.key))
+          const challengeComplete = { ...challenge, ...solvedChallenge.data }
+          const countryInfo = this.countryMappingService.map(challengeComplete.country)
+          const notification: ChallengeSolvedNotification = {
+            key: challenge.key,
+            message: await firstValueFrom(
               this.translateService.get('CHALLENGE_SOLVED', {
                 challenge: challengeComplete.name,
                 description: challengeComplete.description
               })
-            )
-
-            const hasCodingChallenge = challengeComplete.codingChallenge !== undefined
-
-            const notification: ChallengeSolvedNotification = {
-              key: challenge.key,
-              message,
-              flag: challenge.flag,
-              country: countryInfo,
-              copied: false,
-              codingChallengeId: hasCodingChallenge ? challenge.key : undefined
-            }
-
-            this.notifications.unshift(notification)
-            this.ref.detectChanges()
-          } catch (err) {
-            console.log(err)
-          }
-        })
-      }
-    })
-
-    this.socketIoService.on('notification', (data) => {
-      this.ngZone.run(() => {
-        if (data?.message) {
-          const notification: ChallengeSolvedNotification = {
-            key: Math.random().toString(36).substr(2, 9),
-            message: data.message,
+            ),
             flag: '',
             copied: false
           }
           this.notifications.unshift(notification)
           this.ref.detectChanges()
+        } catch (error) {
+          console.error('Error processing challenge:', error)
         }
+      })
+    })
+
+    this.socketIoService.on('notification', (data) => {
+      this.ngZone.run(() => {
+        const notification: ChallengeSolvedNotification = {
+          key: data.key,
+          message: data.message,
+          flag: '',
+          copied: false
+        }
+        this.notifications.unshift(notification)
+        this.ref.detectChanges()
       })
     })
 
@@ -104,17 +102,17 @@ export class ChallengeSolvedNotificationComponent implements OnInit {
     })
 
     this.configurationService.getShowCTFCountryDetailsInNotifications().subscribe((countryMode) => {
-      this.showCtfCountryDetailsInNotifications = countryMode
+      this.showCTFCountryDetailsInNotifications = countryMode
     })
   }
 
   hasCodingChallenge (notification: ChallengeSolvedNotification): boolean {
-    return !!notification.codingChallengeId
+    return !!notification.key
   }
 
   openCodingChallenge (notification: ChallengeSolvedNotification): void {
     this.router.navigate(['/score-board'], {
-      queryParams: { codingChallenge: notification.codingChallengeId }
+      queryParams: { codingChallenge: notification.key }
     })
   }
 
@@ -126,7 +124,7 @@ export class ChallengeSolvedNotificationComponent implements OnInit {
     }
   }
 
-  copyToClipboard (text: string) {
-    this.cookieService.put('continueCode', text)
+  copyToClipBoard(text: string) {
+    this.cookieService.set('continueCode', text)
   }
 }
